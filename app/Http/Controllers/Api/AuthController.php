4a4fadcf0fd8a36e5f24afd5a6e4e4a0f25804f0
Alexandra Galarza <alexandragalarzag@gmail.com>
2022-01-07 13:19:33 +0100

    AuthController inside Api folder

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Validation\ValidationException;
